196a8bafe26500325d8961a5741840a5273801dd
ImeshGimantha <imeshgimantha09@gmail.com>
2026-01-02 17:43:58 +0530

    add database config and initialize express app

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import 'dotenv/config';
+import connectDB from "./config/db.config.js";
+
+const PORT = process.env.PORT || 4000;
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`server running on http://localhost/${PORT}/`);
+});
