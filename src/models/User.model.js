034025e793c409bcbce030a7381f24437b3c4fcd
ImeshGimantha <imeshgimantha09@gmail.com>
2026-01-07 16:11:45 +0530

    add more fields to user model

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     user_id: { type: String, required: true },
-    name: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
     email: { type: String, required: true },
+    country: { type: String, required: true },
     mobile: { type: Number, required: true },
 }, {
     timestamps: true,
-    minimize: false
 });
 
 const User = mongoose.models.user || mongoose.model('user', userSchema);
